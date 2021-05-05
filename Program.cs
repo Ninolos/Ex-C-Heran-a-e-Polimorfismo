0176deada60a1ca79531a7f121a8ad8bc6a0d276
Jonathan Loth <ninoloth@gmail.com>
2021-05-04 22:33:05 -0300

    Exercicio Aula C# Herança e Polimorfismo

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Exercicio_Hrança_e_Polimorfismo.Entities;
+using System.Globalization;
+namespace Exercicio_Hrança_e_Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the Number of Employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data: ");
+                Console.Write("Outsouced (y/n)?");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value Per Hour ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(ch == 'y')
+                {
+                    Console.Write("Additional Charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Payments");
+                foreach(Employee emp in list)
+                {
+                    Console.WriteLine(emp.Name + " - $" + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+                }
+
+            }
+
+        }
+    }
+}
